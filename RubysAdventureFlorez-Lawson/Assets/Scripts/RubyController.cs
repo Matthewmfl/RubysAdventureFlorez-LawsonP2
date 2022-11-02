91159802735989c267091d58c3564a3679db973a
Matthewmfl <735518@student.dusd.net>
2022-11-02 09:55:08 -0700

    Finished Keyboard Input
    Finished character controller and keyboard input and started tilemaps.

--- RubysAdventureFlorez-Lawson/Assets/Scripts/RubyController.cs
+++ RubysAdventureFlorez-Lawson/Assets/Scripts/RubyController.cs
@@ -9,14 +9,15 @@ public class RubyController : MonoBehaviour
     {
         
     }
-
-    // Update is called once per frame
+   // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        Debug.Log(horizontal);
+        float vertical = Input.GetAxis("Vertical");
+
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
         transform.position = position;
         
     }
